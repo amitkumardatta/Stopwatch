74e62a22d1847973baa64ec777f30b5e88f6bcac
amitkumardatta <84867776+amitkumardatta@users.noreply.github.com>
2021-06-25 18:38:17 +0600

    Add files via upload

---
+++ Stopwatch.py
@@ -0,0 +1,12 @@
+
+print("enter number of seconds")
+import time
+a=int(input())
+while a!=0:
+  print(str(a) + "  seconds remaining")
+  time.sleep(1)
+  a=a-1
+print ("the time is up ")
+time.sleep(1)
+
+print ("created by Amit Kumar Datta")
